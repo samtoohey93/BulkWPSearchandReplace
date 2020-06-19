988805719188354346542926c11cb5622ff28fae
Sam Toohey <hello@samtoohey.xyz>
2020-06-19 21:02:06 +1000

    JavaScript changes

--- App/_javascript/main.js
+++ App/_javascript/main.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello Bulma!');
 });
+
+
+//Modal button function for input selection
+const inputs = document.querySelectorAll('input') // Get all the inputs
+const inputsArray = Array.from(inputs) // Gotta convert it to an array and not a nodelist
+const inputValues = inputsArray.map(input => input.value) // Get just their values
+const combinedText = inputValues.join(', ') //Combine
