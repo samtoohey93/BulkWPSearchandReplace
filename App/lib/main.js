cf589b751f7cff24f3a48516196a3cb66d36d409
Sam Toohey <hello@samtoohey.xyz>
2020-06-19 10:25:35 +1000

    Styling and code changes

--- App/lib/main.js
+++ App/lib/main.js
@@ -5,4 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-//Button Addition JavaScript
+//Modal Output function
+
+document.getElementsByClassName("input").value = "X";
+
+// Basically, the up to 10 strings need to have their input fields concatinated. from there, that output this then copied into the modal box and shown as a CLI output command
